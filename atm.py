fa9a5d883a9c172b2e0a11242492fe18e39ba139
Riyaansh1009 <64411382+Riyaansh1009@users.noreply.github.com>
2021-06-15 14:47:40 +0530

    Add files via upload

---
+++ atm.py
@@ -0,0 +1,43 @@
+class Bank_Account:
+    def __init__(self):
+        self.balance=0
+        print("Hello!!! Welcome to the Deposit & Withdrawal Machine")
+     
+    def deposit(self):
+          amount= int(input("Enter amount you want to deposit:"))
+          self.balance = self.balance + amount
+          print("\n Amount Deposited:",amount)
+    def withdraw(self):
+        amount = float(input("Enter amount to be Withdrawn: "))
+        if self.balance>=amount:
+            self.balance-=amount
+            print("\n You Withdrew:", amount)
+        else:
+            print("\n Insufficient balance  ")
+  
+    def display(self):
+        print("\n Net Available Balance=",self.balance)
+
+s = Bank_Account()
+s.deposit()
+s.withdraw()
+s.display()
+
+  
+     
+
+
+     
+     
+
+          
+
+
+
+            
+
+    
+
+
+
+
